5a63ed182e4540ccc8822717b5910733a8cb5744
MshSiam <siyamkenway@gmail.com>
2022-09-15 21:11:28 +0600

    login

--- src/components/page/Login.js
+++ src/components/page/Login.js
@@ -1,45 +1,56 @@
 import React from "react"
 import { useState } from "react"
-import { Button, Col, Container, Form, Row } from "react-bootstrap"
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import "./login.css"
+import { useLoginMutation } from "../../services/appApi"
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [login, { isError, isLoading, error }] = useLoginMutation()
+  function handleLogin(e) {
+    e.preventDefault()
+    login({ email, password })
+  }
+
   return (
     <Container>
       <Row>
         <Col md={6} className="login__form--container">
-          <Form style={{ width: "100%" }} onSubmit="">
+          <Form style={{ width: "100%" }} onSubmit={handleLogin}>
             <h1>Login to your account</h1>
-
-            <Form.Group className="my-2">
+            {isError && <Alert variant="danger">{error.data}</Alert>}
+            <Form.Group>
               <Form.Label>Email Address</Form.Label>
               <Form.Control
                 type="email"
-                placeholder="enter email"
+                placeholder="Enter email"
                 value={email}
+                required
                 onChange={(e) => setEmail(e.target.value)}
-                required></Form.Control>
+              />
             </Form.Group>
 
-            <Form.Group className="my-2">
+            <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type="password"
-                placeholder="enter password"
+                placeholder="Enter Password"
                 value={password}
-                onChange={(e) => setPassword(e.target.password)}
-                required></Form.Control>
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
 
-            <Button className="my-2" type="submit">
-              Login
-            </Button>
-            <p>
-              Don't have an account ?{" "}
-              <Link to="/signup">Create an account</Link>
+            <Form.Group>
+              <Button type="submit" disabled={isLoading}>
+                Login
+              </Button>
+            </Form.Group>
+
+            <p className="pt-3 text-center">
+              Don't have an account? <Link to="/signup">Create account</Link>{" "}
             </p>
           </Form>
         </Col>
---
+++ src/components/page/NewProduct.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./newproduct.css"
+
+const NewProduct = () => {
+  return <div></div>
+}
+
+export default NewProduct
